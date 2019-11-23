cea8039d6f1be650ebf92e586de401e95ac2c9f5
harishpython1 <harish.python.com@gmail.com>
2019-11-23 11:06:59 +0530

    My First Commit

---
+++ src/test/java/com/acti/Demo.java
@@ -0,0 +1,17 @@
+package com.acti;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void runTest() {
+		Reporter.log("Test Runner1 Msg",true);
+	}
+	
+	@Test
+	public void runTest1() {
+		Reporter.log("Test Runner2 Msg",true);
+	}
+}
---
+++ src/test/java/com/acti/Remo.java
@@ -0,0 +1,12 @@
+package com.acti;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Remo {
+
+	@Test
+	public void runTest() {
+		Reporter.log("Test Runner Remo Msg",true);
+	}
+}
